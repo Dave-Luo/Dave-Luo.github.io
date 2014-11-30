df6359887cf1d1fbac4f7fae86122fb837c40aff
luozhangming <kitsudo163@163.com>
2014-11-30 21:32:42 +0800

    更新脚本

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
 chcp 65001
+set PYTHON_HOME=C:\lib\Python27
+set PATH=C:\software\Ruby193\bin;%PYTHON_HOME%;%PYTHON_HOME%\scripts
 jekyll build -w
--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
-start cmd /c "chcp 65001 & jekyll serve --watch"
+start cmd /c "chcp 65001 & set PYTHON_HOME=C:\lib\Python27 & set PATH=C:\software\Ruby193\bin;%PYTHON_HOME%;%PYTHON_HOME%\scripts & jekyll serve --watch"
 ping -n 5 localhost > nul & start http://localhost:4000
